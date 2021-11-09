efc1cd9b7aca2aab1c851f59cf06793350f1d9d4
David Li <davidli012345@gmail.com>
2021-11-09 02:52:22 +0000

    fix: pipeline issue

--- es_cal/browser/nasdaq_ipo_dates.py
+++ es_cal/browser/nasdaq_ipo_dates.py
@@ -30,7 +30,10 @@ priced.to_csv("artifacts/priced.csv")
 def mapIpoForDiscord(item: dict):
     date = item.get("Expected IPO Date")
     if date == None:
-        date = item.get("Date")
+        try:
+            date = item.get("Date")
+        except Exception as e:
+            date = "N/A"
     content = f"{item['Company Name']} {date}"
     embed = {
         'title': item["Symbol"],
@@ -44,7 +47,7 @@ def mapIpoForDiscord(item: dict):
             },
             {
                 'name': "date",
-                "value": item['Date'],
+                "value": date,
                 "inline": True
             },
         ]
